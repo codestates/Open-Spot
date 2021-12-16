cdf59ccdbeeaac02b07870f5c35989368889eeca
PippasSong <tlmag2108@gmail.com>
2021-12-16 23:56:22 +0900

    feat, docs : add sign up function, modify API doc, modify User model
    - API문서의 회원가입 부분 추가, response 부분 수정
    - 테이블 컬럼 추가로 User 모델에도 변경사항 반영
    resolves #53

--- Server/App.js
+++ Server/App.js
@@ -2,6 +2,7 @@ const { swaggerUi, swaggerSpec } = require('./SwaggerDoc');
 const express = require('express');
 const cors = require('cors');
 const markersRouter = require('./Routes/Markers');
+const usersRouter = require('./Routes/Users');
 
 const app = express();
 const port = 80;
@@ -19,6 +20,7 @@ app.use(
 
 app.get('/', (_, res) => res.send('Hello world'));
 app.use('/markers', markersRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
