0c6c7c349d58a93ff1669920a812ebd19841a0fa
PippasSong <tlmag2108@gmail.com>
2021-12-17 22:03:04 +0900

    feat : add signin function
    - 로그인 기능 작업중

--- Server/Routes/Auth.js
+++ Server/Routes/Auth.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const authController = require('../Controllers/AuthController');
 
+router.post('/local', authController.getAllMarkers);
 
 module.exports = router;
