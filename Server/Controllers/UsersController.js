61eb5587ac49b43e93dddede05fbc6001a89d4b3
PippasSong <tlmag2108@gmail.com>
2021-12-18 22:06:12 +0900

    feat, chore, docs : add user info change function, add dependency, modifiy API docs
    close #5

--- Server/Controllers/UsersController.js
+++ Server/Controllers/UsersController.js
@@ -1,5 +1,5 @@
 const models = require('./../models');
-const { createSaltedPassword, createSalt } = require('./HashFunctions');
+const { createSaltedPassword, createSalt, verifyToken } = require('./HashFunctions');
 
 module.exports = {
   signUp: async (req, res) => {
@@ -56,5 +56,45 @@ module.exports = {
         res.status(409).json({ code: 409, error: 'user info already exists' });
       }
     }
+  },
+  changeUserInfo: async (req, res) => {
+    // 이름, 이메일, 비밀번호를 받는다
+    const { newUserName, newEmail, newPassword } = req.body;
+    if (!newUserName || !newEmail || !newPassword || !req.cookies) return res.status(400).json({ code: 400, error: 'bad request' });
+
+    const token = req.cookies.accessToken;
+    // 토큰의 유효성 검사
+    const decoded = await verifyToken(token, process.env.ACCESS_SECRET)
+      .catch(err => {
+        console.log(err);
+        res.cookie('accessToken', '');
+        return res.status(401).json({ code: 401, error: 'expired token' });
+      });
+
+    // id정보로 유저 조회
+    const userInfo = models.User.findOne({ where: { id: decoded.id } }).catch(err => {
+      console.log(err);
+      return res.status(404).json({ code: 404, error: 'user not found' });
+    });
+
+    // 암호화된 비밀번호 생성
+    const salt = await createSalt();
+    const { saltedPassword } = await createSaltedPassword(newPassword, salt).catch(err => {
+      return res.status(500).json({ code: 500, error: err });
+    });
+
+    // 레코드 수정
+    userInfo.set({
+      userName: newUserName,
+      email: newEmail,
+      saltedPassword: saltedPassword,
+      salt: salt
+    });
+
+    await userInfo.save().catch(err => {
+      return res.status(500).json({ code: 500, error: err });
+    });
+
+    res.status(201).json({ code: 201, message: 'modified' });
   }
 };
