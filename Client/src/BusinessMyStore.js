de069501de73cfed9e6f32e3b07cd8ce0f470c09
Brian <cowk34209@gmail.com>
2021-12-19 15:27:32 +0900

    refact : modify business my store
    flex 부분 수정
    Ref #79

--- Client/src/BusinessMyStore.js
+++ Client/src/BusinessMyStore.js
@@ -6,7 +6,7 @@ function BusinessMyStore () {
     <div className="entire-box">
       <div className="intro-bg">
         <div className="header">
-          <div className="rogo">로고</div>
+          <div id="logo" />
           <ul className="nav">
             <li><a href="www.naver.com">MAP</a></li>
             <li><a href="www.naver.com">MYPAGE</a></li>
@@ -34,6 +34,24 @@ function BusinessMyStore () {
           </div>
           <div className="aside"></div>
           <div className="divider">
+            <div className="store-info">
+              <div className="store-img-box">
+                <div className="store-img"></div>
+              </div>
+              <div className="store-name">니쿠킹 버거</div>
+              <div className="store-detail-info">
+                <div className="detail-info">
+                  <div>주소 : 서울 강서구 강서로 72-5 580번지 코드스테이츠 1층</div>
+                  <div>전화번호 : 010-0000-0000</div>
+                  <div>주차여부 : 5/20</div>
+                  <div>예약 여부 : 가능</div>
+                </div>
+                <div className="reaction-box">
+                  <div>💜</div>
+                  <div className="count">20</div>
+                </div>
+              </div>
+            </div>
             <div className="store-info">
               <div className="store-img-box">
                 <div className="store-img"></div>
