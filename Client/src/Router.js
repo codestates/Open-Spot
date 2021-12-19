914c826bb32cba84e7e1e5248fca39aaeec63506
ciocio97 <tmddusleo@icloud.com>
2021-12-19 17:56:49 +0900

    feat : change link by using redux
    Ref #83

--- Client/src/Router.js
+++ Client/src/Router.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import * as React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from 'react-router-dom';
 
-import Nav from './Components/Nav.js';
-import Home from './Pages/Home.js';
-import Login from './Pages/Login.js';
-import Signin from './Pages/Signin.js';
+import Home from './Pages/Home.js'; // Home을 Routes위에 안넣어줘도 맞아 이게 ?
+import Switch from './Pages/Switch.js';
+import ClientLogin from './Pages/ClientLogin.js';
+import ClientSignin from './Pages/ClientSignin.js';
+import BusinessLogin from './Pages/BusinessLogin.js';
+import BusinessSignin from './Pages/BusinessSignin.js';
+
+// for redux
+import { selectLoginOrSignin } from './Actions/index.js';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Routers () {
+  const state = useSelector(state => state.pageReducer);
+  const { isLoginTab } = state;
+  const dispatch = useDispatch();
+
+  const handleGuestClick = (bool) => {
+    dispatch(selectLoginOrSignin(bool));
+  };
+  // const [isLoginTab, setIsLoginTab] = useState(null);
+  // const clickLoginTab = () => {
+  //   setIsLoginTab(true);
+  //   console.log(isLoginTab);
+  // };
+  // const clickSigninTab = () => {
+  //   setIsLoginTab(false);
+  //   console.log(isLoginTab);
+  // };
   return (
     <Router>
-      <Nav />
-        <Routes>
-          <Route exact={true} path='/' element={<Home />} />
-          <Route path='/login' element={<Login />}/>
-          <Route path='/signin' element={<Signin />}/>
-        </Routes>
+      <Routes>
+        <Route exact={true} path='/' element={<Home check1={handleGuestClick} />} />
+        <Route path='/switch' element={<Switch isLoginTab={isLoginTab} />} />
+          <Route path='/client/login' element={<ClientLogin />}/>
+          <Route path='/business/login' element={<BusinessLogin />} />
+          <Route path='/client/signin' element={<ClientSignin />} />
+          <Route path='/business/signin' element={<BusinessSignin />} />
+      </Routes>
     </Router>
   );
 }
