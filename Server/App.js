aba82d824d19b0412413b7a1c0c6f5abca78a07a
jyang510 <yayww@naver.com>
2021-12-21 16:45:03 +0900

    feature: add verify password when withdrawal, fix: oauth or not
    회원 탈퇴시 비밀번호 검증해야 하는 부분을 구현했습니다.
    추가로 로그인시 Oauth 회원인가 아닌가 여부도 리턴하게 바꿨습니다
    Ref #101

--- Server/App.js
+++ Server/App.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const markersRouter = require('./Routes/Markers');
 const authRouter = require('./Routes/Auth');
 const usersRouter = require('./Routes/Users');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 80;
@@ -14,7 +13,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cookieParser());
 app.use(
   cors({
     origin: true,
