66a0afec550be471dad09235c101a85aa9b8c130
Brian <85025833+Brian-free1@users.noreply.github.com>
2021-12-22 09:40:50 +0900

    Update index.js

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import store from './Store/Store.js';
 import { Provider } from 'react-redux';
 // test
 import Router from './Router.js';
-// import MapUser from './Pages/MapUser.js';
-// import MapGuest from './Pages/MapGuest.js';
-// import reportWebVitals from './reportWebVitals';
-// import Login from './Login.js';
-// import './AllMyPage.css';
-// import ClientMyPage from './MyPages/ClientMyPage.js';
-// import ClientFavoriteList from './ClientFavoriteList.js';
-// import BusinessFavoriteList from './BusinessFavoriteList.js';
-// import BusinessMyPage from './BusinessMyPage.js';
-// import BusinessMyStore from './BusinessMyStore.js';
-// import MyPageRouter from './MyPageRouter.js';
-// import ClientMyPageRouter from './ClientMyPageRouter.js';
-// import WithDrawal from './Modals/WithDrawal';
-// import ModalBox from './Modals/ModalBox';
-// import RegisterStore from './Modals/RegisterStore';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={ store }>
