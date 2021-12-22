8a1d68deb16be42b44553b71cef0dd50f0b3660d
PippasSong <tlmag2108@gmail.com>
2021-12-23 01:14:00 +0900

    fix : modify sign up response, fix business my store
    ref #53

--- Client/src/Components/SocialLoginURL.js
+++ Client/src/Components/SocialLoginURL.js
@@ -4,7 +4,7 @@ const GOOGLE_CLIENT_ID = '240166666352-jrcs8avt11vl8dp0hgo60qgnhf2t9rnn.apps.goo
 const GOOGLE_AUTHORIZE_URI = 'https://accounts.google.com/o/oauth2/v2/auth';
 const GOOGLE_QUERY_STRING = qs.stringify({
   client_id: GOOGLE_CLIENT_ID,
-  redirect_uri: 'https://d1839m99iakp36.cloudfront.net',
+  redirect_uri: 'https://open-spot.tk',
   response_type: 'code',
   scope: 'openid profile email',
   access_type: 'offline'
@@ -14,7 +14,7 @@ const NAVER_CLIENT_ID = 'cwNXt0ItxMlOlF3gnPSl';
 const NAVER_AUTHORIZE_URI = 'https://nid.naver.com/oauth2.0/authorize';
 const NAVER_QUERY_STRING = qs.stringify({
   client_id: NAVER_CLIENT_ID,
-  redirect_uri: 'https://d1839m99iakp36.cloudfront.net',
+  redirect_uri: 'https://open-spot.tk',
   response_type: 'code',
   state: 'hello'
 });
@@ -23,7 +23,7 @@ const KAKAO_CLIENT_ID = 'dca677be4251f006b061960a3063b1f4';
 const KAKAO_AUTHORIZE_URI = 'https://kauth.kakao.com/oauth/authorize';
 const KAKAO_QUERY_STRING = qs.stringify({
   client_id: KAKAO_CLIENT_ID,
-  redirect_uri: 'https://d1839m99iakp36.cloudfront.net',
+  redirect_uri: 'https://open-spot.tk',
   response_type: 'code'
 });
 
